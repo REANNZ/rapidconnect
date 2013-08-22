a1a0a87c9bf49d1d77f4f1b87abd8331317e91dc
Bradley Beddoes <bradleybeddoes@gmail.com>
2013-08-22 10:26:20 +1000

    Provides script to grab data from FR and store in local filesystem.
    To be run from cron at regular intervals.

---
+++ frdatacollator/collator.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+require 'addressable/uri'
+require 'json'
+require 'yaml'
+
+##
+# Grab all organisations currently in FR for users to select when registering services
+##
+def secure_server_request(uri)
+  http = Net::HTTP.new @config['server'], 443
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  req = Net::HTTP::Get.new uri
+  response = http.request req
+
+  case response
+    when Net::HTTPSuccess
+      response.body
+    else
+      raise "Invalid FR response for API call #{response.value}"
+  end
+end
+
+@config = YAML.load_file('config.yml')
+
+org_names = []
+fr_response = secure_server_request Addressable::URI.encode @config['org_api_endpoint']
+fr_json = JSON.parse fr_response
+
+fr_json["organizations"].each { |org|
+  fr_org_json = JSON.parse(secure_server_request org['link'])
+  org_names << fr_org_json['organization']['displayName']
+}
+
+File.open('/tmp/fr_org_names.json.new', 'w') {|f| f.write( JSON.generate org_names.sort! ) }
+
