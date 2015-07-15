c149e1d57e4e580a3584337b462468ed4606417e
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-07-15 14:50:28 +1000

    Fix deprecation warnings from test suite

--- rapidconnect/spec/models/rapid_connect_service_spec.rb
+++ rapidconnect/spec/models/rapid_connect_service_spec.rb
@@ -18,7 +18,7 @@ describe RapidConnectService do
     it { is_expected.to validate_presence_of(:registrant_mail) }
 
     it { is_expected.to validate_presence_of(:secret) }
-    it { is_expected.to ensure_length_of(:secret).is_at_least(16) }
+    it { is_expected.to validate_length_of(:secret).is_at_least(16) }
 
     it { is_expected.to validate_presence_of(:audience) }
     it { is_expected.to allow_value('http://example.com').for(:audience) }
@@ -109,7 +109,8 @@ describe RapidConnectService do
 
     it 'fails on an invalid attribute' do
       bad_data = attrs.merge('unknown_attribute' => 'value').to_json
-      expect { RapidConnectService.new.from_json(bad_data) }.to raise_error
+      expect { RapidConnectService.new.from_json(bad_data) }
+        .to raise_error(/Bad attribute/)
     end
 
     # This checks compatibility with the format used by Rapid Connect up to
