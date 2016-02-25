1243d99f01f9529eb9df52726e875e818a923b1d
Bradley Beddoes <bradleybeddoes@gmail.com>
2016-02-25 15:25:43 +1000

    Remove need for scoped_affilliation
    This change needs to be reverted once #43 is ready to be satisfied.
    
    Closes #42.

--- rapidconnect/spec/rapid_connect_spec.rb
+++ rapidconnect/spec/rapid_connect_spec.rb
@@ -215,7 +215,10 @@ describe RapidConnect do
         let(:invalid_headers) do
           dup_headers_and_remove_exisiting('HTTP_AFFILIATION')
         end
-        include_examples 'halts invalid user session'
+        it 'does not halt session, continues' do
+          expect(last_response).to be_redirect
+          expect(last_response.location).not_to eq(invalid_session_target)
+        end
       end
     end
   end
