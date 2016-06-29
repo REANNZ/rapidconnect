e3ce9923972f2b10a292692a26146efae2bd8529
Bradley Beddoes <bradleybeddoes@gmail.com>
2016-06-29 15:50:21 +1000

    Migrate specs to Capybara and latest RSpec/Rubocop
    This commit bundles a few changes as they (unfortuantely) got a run on:
    
    * Use Capybara matchers within specs, not Webrat
    * Comply to latest Rubocop requirements
    * Some minor adjustments to support latest RSpec release

--- rapidconnect/spec/bin/rapidconnect_spec.rb
+++ rapidconnect/spec/bin/rapidconnect_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 load './bin/rapidconnect'
 
 describe RapidConnectCLI do
--- rapidconnect/spec/models/attributes_claim_spec.rb
+++ rapidconnect/spec/models/attributes_claim_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require './app/models/attributes_claim'
 
 RSpec.describe AttributesClaim do
