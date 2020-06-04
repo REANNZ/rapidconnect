af41f21acb4ec2d26e00fac950256013f46438dc
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2020-06-04 14:46:24 +1200

    Mail: disable opportunistic STARTTLS (localhost)
    Both RapidConnect and the Mail library default to opportunistic STARTTLS - use if supported.
    
    On CentOS, Postfix does not support STARTLS so this is ignored.
    
    On Ubuntu with REANNZ baseline applied, exim4 supports STARTTLS, but uses a
    self-signed cert issued in the name hosts FQDN - which then does not match
    "localhost" (and would not be trusted anyway because of being self-signed).
    
    While this is not ideal situation, work around this by disabling STARTTLS - the connections
    are hard-coded to localhost anyway.

--- rapidconnect/app/rapid_connect.rb
+++ rapidconnect/app/rapid_connect.rb
@@ -61,7 +61,7 @@ class RapidConnect < Sinatra::Base
                       user_name: mail_settings[:user_name],
                       password: mail_settings[:password],
                       authentication: :plain,
-                      enable_starttls_auto: true
+                      enable_starttls_auto: false
     end
 
     unless settings.respond_to? :hostname
