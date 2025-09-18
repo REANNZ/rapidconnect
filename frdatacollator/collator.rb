498f89afc80331414153fdc7982bc13e59b872ff
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2025-09-18 18:06:11 +1200

    fix: frdatacollator: use select instead of "next" to restrict to functioning organisations
    Use a native Ruby way of doing this.

--- frdatacollator/collator.rb
+++ frdatacollator/collator.rb
@@ -27,8 +27,7 @@ org_names = []
 fr_response = secure_server_request Addressable::URI.encode @config['org_api_endpoint']
 fr_json = JSON.parse fr_response
 
-fr_json["organizations"].each { |org|
-  next unless org['functioning']
+fr_json["organizations"].select { |org| org['functioning'] }.each { |org|
   fr_org_json = JSON.parse(secure_server_request org['link'])
   org_names << fr_org_json['organization']['displayName']
 }
